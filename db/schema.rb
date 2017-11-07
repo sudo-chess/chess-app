0f50d636d02f6eef213bc2fd7dcbda9b0f68664a
cxm6467@gmail.com <cxm6467@gmail.com>
2017-11-07 19:31:29 +0000

    DB model for games

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171107192445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.integer  "game_id"
+    t.integer  "white_player_id"
+    t.integer  "black_player_id"
+    t.integer  "result"
+    t.integer  "winner_id"
+    t.integer  "loser_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
