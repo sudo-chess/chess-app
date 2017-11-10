bbac1f9a0f37aeab136e1aaa082e6d8def22bb0d
Guillaume Bihet <guillaume.bihet@gmail.com>
2017-11-10 15:51:11 +0000

    added missing dependancies to Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,7 @@
 class Game < ApplicationRecord
   has_many	:pieces
 
+  belongs_to :white_player, :class_name => 'User', :foreign_key => 'white_player_id'
+  belongs_to :black_player, :class_name => 'User', :foreign_key => 'black_player_id'
+
 end
