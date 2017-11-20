7d638c521c79caf57aaa3914ef50c7383efad877
Guillaume Bihet <guillaume.bihet@gmail.com>
2017-11-20 08:46:40 +0000

    FactoryBot implemented for :game and is_obstructed? tests

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -11,17 +11,10 @@ RSpec.describe GamesController, type: :controller do
   describe "is_obstructed?" do
     it "should return true because it is obstructed" do
       
-
       game = FactoryBot.create(:game)
-      # game = FactoryBot.build(:game, white_player_id: 1, black_player_id: 2)
-      
-      # user1= User.create( email: 'fake@fake.fake', password: 'password')
-      # user2= User.create( email: 'fake3@fake.fake', password: 'password')
-      # game=Game.create(white_player_id: user1.id, black_player_id: user2.id)
+ 
       piece1 = game.pieces.create(position_x: 7, position_y: 7, game_id: game.id)
       piece2 = game.pieces.create(position_x: 7, position_y: 5, game_id: game.id)
-      game.pieces << piece1 << piece2 
-
       variable = piece1.is_obstructed?(game,[7,3])
       expect(variable).to eq(true)
 
@@ -31,13 +24,9 @@ RSpec.describe GamesController, type: :controller do
     describe "is_obstructed?" do
     it "should return false because it is not obstructed" do
       
-      user1= User.create( email: 'fake@fake.fake', password: 'password')
-      user2= User.create( email: 'fake3@fake.fake', password: 'password')
-      game=Game.create(white_player_id: user1.id, black_player_id: user2.id)
+      game = FactoryBot.create(:game)
       piece1 = game.pieces.create(position_x: 7, position_y: 7, game_id: game.id)
       piece2 = game.pieces.create(position_x: 2, position_y: 2, game_id: game.id)
-      game.pieces << piece1 << piece2
-
       variable = piece1.is_obstructed?(game,[3,3])
       expect(variable).to eq(false)
     end
@@ -46,13 +35,9 @@ RSpec.describe GamesController, type: :controller do
   describe "is_obstructed?" do
     it "should return false because it is not obstructed, even if destination square is occupied" do
       
-      user1= User.create( email: 'fake@fake.fake', password: 'password')
-      user2= User.create( email: 'fake3@fake.fake', password: 'password')
-      game=Game.create(white_player_id: user1.id, black_player_id: user2.id)
+      game = FactoryBot.create(:game)
       piece1 = game.pieces.create(position_x: 7, position_y: 7, game_id: game.id)
       piece2 = game.pieces.create(position_x: 2, position_y: 2, game_id: game.id)
-      game.pieces << piece1 << piece2
-
       variable = piece1.is_obstructed?(game,[2,2])
       expect(variable).to eq(false)
     end
@@ -61,13 +46,9 @@ RSpec.describe GamesController, type: :controller do
   describe "is_obstructed?" do
     it "should return Invalid statement because it is not a linear move" do
       
-      user1= User.create( email: 'fake@fake.fake', password: 'password')
-      user2= User.create( email: 'fake3@fake.fake', password: 'password')
-      game=Game.create(white_player_id: user1.id, black_player_id: user2.id)
+      game = FactoryBot.create(:game)
       piece1 = game.pieces.create(position_x: 2, position_y: 2, game_id: game.id)
       piece2 = game.pieces.create(position_x: 6, position_y: 6, game_id: game.id)
-      game.pieces << piece1 << piece2
-
       variable = piece1.is_obstructed?(game,[1,7])
       expect(variable).to eq("Invalid input.  Not diagnal, horizontal, or vertical.")
     end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,42 +1,16 @@
 FactoryBot.define do
+  
   factory :user do
     sequence :email do |n|
       "sampleEmails#{n}@gmail.com"
     end
-    # sequence(:white_player_id) { |n| n}
-    # sequence(:black_player_id) { |n| n}
     password "secretPassword"
     password_confirmation "secretPassword"
   end
 
-
   factory :game do 
-    # first_name 'John' 
-    # sequence(:name) { |n| "user#{n}" }
-    # sequence(:email) { |n| "fake#{n}@gamail.com"}
-
-    user1 = FactoryBot.create(:user)
-    user2= FactoryBot.create(:user)
-    white_player_id user1.id
-    black_player_id user2.id
-
-
-    # sequence(:white_player_id) { |n| n}
-    # sequence(:black_player_id) { |n| n}
-    # association :user
+  association :white_player, factory: :user
+  association :black_player, factory: :user
   end
-
-  # factory :game do
-    # game = Game.create(white_player_id: 1, black_player_id: 2)
-
-    # or
-    # game = Game.new
-
-    # piece1 = Piece.create(position_x: 7, position_y: 7, game_id: game.id)
-    # piece2 = Piece.create(position_x: 7, position_y: 5, game_id: game.id)
-    # piece3 = Piece.create(position_x: 2, position_y: 2, game_id: game.id)
-    # game.pieces << piece1 << piece2 << piece3
-  # end
-
 
 end
