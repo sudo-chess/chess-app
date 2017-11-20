0567c3c9c2c53cde319b235892ddb7718f612078
Guillaume Bihet <guillaume.bihet@gmail.com>
2017-11-20 14:42:57 +0000

    FactoryBot for obstructed. Scope added for conflict solving

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,8 @@ class Game < ApplicationRecord
   belongs_to :white_player, :class_name => 'User', :foreign_key => 'white_player_id'
   belongs_to :black_player, :class_name => 'User', :foreign_key => 'black_player_id', optional: true
 
+  scope :pending,  ->{ where(black_player_id: nil)}
+  scope :playing,  ->{ where.not(black_player_id: nil).where(result: nil)}
+  scope :complete, ->{ where(winner_id: true)}
+
 end
