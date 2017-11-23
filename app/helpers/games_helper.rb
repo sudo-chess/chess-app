2f9136bbe3d5501c75bab2fcbe4abf9473a556a1
Guillaume Bihet <guillaume.bihet@gmail.com>
2017-11-23 16:23:58 +0000

    added piece controller show action and view for link on pieces

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,13 +1,14 @@
 module GamesHelper
   
-  def check_piece(x, y)
+ def check_piece(x, y)
     @local_pieces.each do |item|
       if item != nil
         if item.position_x == x && item.position_y == y
-          return "#{item.color} #{item.type}" 
+          
+          return link_to "#{item.color} #{item.type}", piece_path(item)
         end
       end
     end
-    return "#{x}, #{y}"
+    return nil
   end
 end
