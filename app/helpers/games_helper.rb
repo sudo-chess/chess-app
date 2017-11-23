806906236d98dc0c5d9c69a70c02413c9edbdfa8
Kyle Duca <kyleduca@gmail.com>
2017-11-23 14:28:09 +0000

    changed placement

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,14 +1,13 @@
 module GamesHelper
   
   def check_piece(x, y)
-    current_game = @g.id
     @p.each do |item|
       if item != nil
         if item.position_x == x && item.position_y == y
-          return "#{item.color} #{item.type}"
+          return "#{item.color} #{item.type}" 
         end
       end
     end
-    nil
+    "+ link"
   end
 end
