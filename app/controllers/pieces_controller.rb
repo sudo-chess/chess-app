9039c9325e14bdd75359867851657f143698bcca
Guillaume Bihet <guillaume.bihet@gmail.com>
2017-11-23 17:28:20 +0000

    added logic to access current coordinates of a piece in the piece show view

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -4,6 +4,9 @@ class PiecesController < ApplicationController
       @local_game_id = Piece.find_by_id(params[:id]).game_id
       @local_game = Game.find_by_id(@local_game_id)
       @local_pieces = @local_game.pieces
+      @current_piece = Piece.find_by_id(params[:id])
+      @current_coordinates = [@current_piece.position_x, @current_piece.position_y]
+
   end
 
 
--- app/helpers/pieces_helper.rb
+++ app/helpers/pieces_helper.rb
@@ -1,2 +1,36 @@
 module PiecesHelper
+
+
+def highlight
+  @local_pieces.each do |item|
+    if item != nil
+
+      if item.position_x == @current_coordinates[0] && item.position_y == @current_coordinates[1]
+          return "blablabla"
+      end
+    end
+  end
+  return nil
 end
+
+
+ def check_piece(x, y)
+
+    @local_pieces.each do |item|
+      if item != nil
+
+        if item.position_x == x && item.position_y == y
+          return link_to "#{item.color} #{item.type}", piece_path(item)
+        end
+
+      end
+    end
+    return nil
+  end
+end
+
+
+
+ # @current_coordinates == [index, row_number] %>
+ #            <b style="font-weight: bold">df </b>
+    
