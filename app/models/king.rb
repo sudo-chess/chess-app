7e784e539047c183aec69fb0705c769222558a61
IanWei <ikaeuu@gmail.com>
2017-11-24 23:10:36 +0000

    add icons on the chessboard

--- app/models/king.rb
+++ app/models/king.rb
@@ -1,4 +1,11 @@
 class King < Piece
   name = "King"
-  
+
+  def self.get_image(color)
+    if color == "white"
+      return "chess_piece_king_white.png"
+    elsif color == "black"
+      return "chess_piece_king_black.png"
+    end
+  end  
 end
--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,4 +1,12 @@
 class Knight < Piece
 
   name = "Knight"  
+
+  def self.get_image(color)
+    if color == "white"
+      return "chess_piece_knight_white.png"
+    elsif color == "black"
+      return "chess_piece_knight_black.png"
+    end
+  end  
 end
