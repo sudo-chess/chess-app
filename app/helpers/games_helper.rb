9bf6cf096c6f7a47b4c4b09f3e58d1ade1b025f0
Guillaume Bihet <guillaume.bihet@gmail.com>
2017-12-03 10:25:31 +0000

    drag and drop basic

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,15 +1,38 @@
 module GamesHelper
   
+#  def check_piece(x, y)
+#     @local_pieces.each do |item|
+#       if item != nil
+#         if item.position_x == x && item.position_y == y       
+#           # return link_to image_tag(item.image, :class=>"img-responsive", :id =>"draggable"), piece_path(item)
+#           return image_tag(item.image, :class=>"img-responsive", :id =>"draggable")
+#         end
+#       end
+#     end
+#     return nil
+#   end
+# end
+
+
+
  def check_piece(x, y)
-    @local_pieces.each do |item|
-      if item != nil
-        if item.position_x == x && item.position_y == y       
-          return link_to image_tag(item.image, :class=>"img-responsive", :id =>"draggable"), piece_path(item)
+    @local_pieces.each do |piece|
+      if piece != nil
+        if piece.position_x == x && piece.position_y == y
+          # return link_to image_tag(piece.image, :class=>"img-responsive", :id =>"draggable"), piece_path(piece, position_x: x, position_y: y), :method => :put, :id =>"draggable"
+          return image_tag(piece.image, :class=>"img-responsive", :id =>"draggable")
         end
       end
     end
-    return nil
+
+    # return link_to '', piece_path(@current_piece.id, position_x: x, position_y: y), :method => :put, :class => "square", :id => "droppable"
+    # return   :class => "square", :id => "droppable"
+    # return  content_tag("rrr") :div, class: "square", id: "droppable"
+    return  content_tag(:div, "", class: "square", id: "droppable")
+
+    
   end
 end
+
 
 
