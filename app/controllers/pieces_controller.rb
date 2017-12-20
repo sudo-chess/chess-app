b61c829c80b585427fe376f7434d053e79dabbe1
Kyle Duca <kyleduca@gmail.com>
2017-12-20 09:18:42 +0000

    fixed error with invaild capture

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -17,29 +17,22 @@ class PiecesController < ApplicationController
 
       @target_x = piece_params[:position_x].to_i
       @target_y = piece_params[:position_y].to_i
-      @target = @local_game.pieces.where(position_x: @target_x, position_y: @target_y)
+      @target = @local_game.pieces.where(position_x: @target_x, position_y: @target_y).first
       @old_x = @current_piece.position_x
       @old_y = @current_piece.position_y
       
       if @current_piece.valid_move?(@target_x, @target_y)
-        puts "HEEEEEEEEEEEEEEEEEEEERE!!!!!! #{@target.inspect}"
-
         @current_piece.move_to!(@target_x, @target_y)
         king = @local_game.pieces.where(type: "King", color: @current_piece.color)[0]
         if king.is_in_check? == true
           @current_piece.move_to!(@old_x, @old_y)
-          puts "HEEEEEEEEEEEEEEEERE!!!!!!fwsgewgw #{@target.inspect}"
-          if @target != []
-            @target.update_attributes!(position_x: @target_x, position_y: @target_y)
+          if @target != nil
+            @revert = Piece.find_by(id: @target.id)
+            @revert.update_attributes!(position_x: @target_x, position_y: @target_y)
           end
           @local_game.next_player(@local_game.next_player_id)
         end
         @local_game.next_player(@local_game.next_player_id)
-
-
-      # if @current_piece.valid_move?(@target_x, @target_y)
-      #   @current_piece.move_to!(@target_x, @target_y)
-      #   @local_game.next_player(@local_game.next_player_id)
       else
         flash[:notice] = "That was not a valid move"  
       end
